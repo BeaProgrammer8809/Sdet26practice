f23f19cb9f2098bc72cb6fcd11ec3d97938aa3e5
91912 <91912@LAPTOP-NNQ3O8RD>
2021-12-09 22:06:04 +0530

    user 1 created for script 1

---
+++ src/test/java/com/crm/SampleTest.java
@@ -0,0 +1,12 @@
+package com.crm;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void createUser() {
+		System.out.println("user-1 created successfully - script-1");
+	}
+
+}
