514d5ab7e9f0d8feac84a2e575f77be5a22c26e0
GUNNY <GUNNY@DESKTOP-RGL60CC>
2021-12-09 22:21:10 +0530

    User-2 created succesfully script -2

--- src/test/java/com/crm/SampleTest.java
+++ src/test/java/com/crm/SampleTest.java
@@ -7,6 +7,8 @@ public class SampleTest {
 	@Test
 	public void createUser() {
 		System.out.println("user-1 created successfully - script-1");
+		
+		System.out.println("user-2 created succesfully Script-2");
 	}
 
 }
